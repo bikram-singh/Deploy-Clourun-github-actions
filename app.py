c22738559642ea6ee80e824a5fdca69656ac6c67
bikram-singh <144786290+bikram-singh@users.noreply.github.com>
2025-08-07 18:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, This service is deploy using GitHub Actions on Cloud Run_v1!"
+    return "Hello, This service is deploy using GitHub Actions on Cloud Run_v2!"
